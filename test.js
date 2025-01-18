cd4c0d18d41a2ab1fd4b5af18083fafdd30af80a
DARIAS66 <david.arias.p@gmail.com>
2025-01-18 00:02:19 -0600

    Creación de un object y que se visualice en el index

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 console.log("Imprimiendo que estoy manejando GIT")
+const persona = {
+    nombre: "Juan",
+    edad: 30,
+    genero: "Masculino",
+    ocupacion: "Desarrollador",
+    nacionalidad: "Española"
+};
+
+console.log(persona);
+document.body.innerHTML = `<pre>${JSON.stringify(persona, null, 2)}</pre>`;
